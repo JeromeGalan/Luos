770080c5a0165e12644a29a91c7b08c97d765828
nicolas cros <nicolas.cros@luos.io>
2021-05-18 10:06:28 +0200

    fix code format

--- src/boot.c
+++ src/boot.c
@@ -12,7 +12,10 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#define REV {1,0,0}
+#define REV     \
+    {           \
+        1, 0, 0 \
+    }
 
 /*******************************************************************************
  * Variables
@@ -23,10 +26,10 @@ typedef enum
 } App_type_t;
 
 uint16_t boot_blinktime = 100;
-unsigned long boot_my_time; //Used to keep track of time
+unsigned long boot_my_time;      //Used to keep track of time
 uint8_t boot_led_last_state = 0; //Is the LED on or off?
-uint8_t boot_count = 0;
-uint8_t nb_blink = 3;
+uint8_t boot_count          = 0;
+uint8_t nb_blink            = 3;
 
 /*******************************************************************************
  * Function
@@ -40,7 +43,7 @@ static void Boot_MsgHandler(container_t *container, msg_t *msg);
  ******************************************************************************/
 void change_blink(uint8_t new_nb_blink)
 {
-  nb_blink = new_nb_blink;
+    nb_blink = new_nb_blink;
 }
 
 /******************************************************************************
@@ -50,8 +53,8 @@ void change_blink(uint8_t new_nb_blink)
  ******************************************************************************/
 void Boot_Init(void)
 {
-	revision_t revision = {.unmap = REV};
-  boot_container = Luos_CreateContainer(Boot_MsgHandler, BOOT_APP, "boot_app", revision);
+    revision_t revision = {.unmap = REV};
+    boot_container      = Luos_CreateContainer(Boot_MsgHandler, BOOT_APP, "boot_app", revision);
 }
 /******************************************************************************
  * @brief loop must be call in project loop
@@ -60,31 +63,31 @@ void Boot_Init(void)
  ******************************************************************************/
 void Boot_Loop(void)
 {
-  //Check to see if we have overshot our counter
-	if (boot_my_time < HAL_GetTick())
-	{
-    //Reset the counter
-    boot_my_time = HAL_GetTick() + boot_blinktime;
-
-    if (boot_count < nb_blink)
+    //Check to see if we have overshot our counter
+    if (boot_my_time < HAL_GetTick())
     {
-      boot_count++;
-      boot_blinktime = 100;
-    }
-    else
-    {
-      boot_count = 0;
-      boot_blinktime = 1000;
-    }
+        //Reset the counter
+        boot_my_time = HAL_GetTick() + boot_blinktime;
 
-    //Invert the LED state
-    boot_led_last_state = (boot_led_last_state == 1 ? 0: 1);
+        if (boot_count < nb_blink)
+        {
+            boot_count++;
+            boot_blinktime = 100;
+        }
+        else
+        {
+            boot_count     = 0;
+            boot_blinktime = 1000;
+        }
 
-    if (boot_led_last_state)
-      HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-    else
-      HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
-	}
+        //Invert the LED state
+        boot_led_last_state = (boot_led_last_state == 1 ? 0 : 1);
+
+        if (boot_led_last_state)
+            HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+        else
+            HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+    }
 }
 /******************************************************************************
  * @brief Msg manager callback when a msg receive for this container
