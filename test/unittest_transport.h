a7e6ecc615e37ec2a86964280bf8b4fb3e6f1cac
GGG_33 <79972092+JeromeGalan@users.noreply.github.com>
2021-06-03 22:02:19 +0200

    clang format code

--- test/unittest_transport.h
+++ test/unittest_transport.h
@@ -3,20 +3,22 @@
 
 #include <stdio.h>
 
-void unittest_uart_begin() {
-
+void unittest_uart_begin()
+{
 }
 
-void unittest_uart_putchar(char c) {
-  putchar(c);
+void unittest_uart_putchar(char c)
+{
+    putchar(c);
 }
 
-void unittest_uart_flush() {
-  fflush(stdout);
+void unittest_uart_flush()
+{
+    fflush(stdout);
 }
 
-void unittest_uart_end() {
-
+void unittest_uart_end()
+{
 }
 
 #endif
